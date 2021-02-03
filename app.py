30443caa2c006e083f991a9e32f71db41ce0ae84
minhnhatnguyen2003 <47096001+minhnhatnguyen2003@users.noreply.github.com>
2021-02-03 15:20:45 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+
+import streamlit as st
+import time
+
+st.title("Hello Đăng :point_right: :point_left:")
+
+text = 'HaiDang,Confident,Smart,Talented,Passionate,Humble,LowkeyCrazy,Horny,Caring,FutureDoctor,Kind,Memelord,Uvuvwevwevwe onyetenyevwe ugwemuhwem osas, 1_Kilo_of_feathers, Why_are_u_gay'
+
+
+
+if st.button("Press me"):
+  with st.spinner("Why am I showing you this? :eyes:"):
+    time.sleep(3)
+    st.subheader(":red_circle: I admire your light and your warmth; they make everyday brighter.")
+    st.subheader(":red_circle: I admire your drive and intelligence. I have no doubt that you'll achieve great things.")
+    st.subheader(":red_circle: I love your energy.How the hell can you do a morning class, basketball and football in a day?!?! You amaze me in every way")
+    st.subheader(":red_circle: YOU MADE ME A BETTER PERSON")
+    st.write('So I decided to make you some custom memes')
+    st.write('Cause this is me tripping for u')
+    st.video('https://youtu.be/u2e9u1Oyl7o')
+    st.image("https://i.pinimg.com/564x/0b/70/37/0b7037498e232cb64a8185b55eccb226.jpg", use_column_width=True, caption = 'I made this WordCloud abt what I think of you')
+    st.image("https://i.pinimg.com/originals/13/aa/42/13aa42c14657034f5e76981bb8ce87ae.png?epik=dj0yJnU9U1l3UWVlMWlCRmVDdWduVUlEWExVMUhKQTJOLXR4VzQmcD0wJm49WDhsSjhBZ0dYNk9rVm95cTRyLUlWZyZ0PUFBQUFBR0FhS1Vz.png", use_column_width=True, caption = 'I also made this meme code with sum code ofc ')
+
+
+if st.button("Hit me second"):
+            st.balloons()
+            time.sleep(1.5)
+            st.subheader("Sooooooo.....  ")
+            st.subheader("Will you be my mập địt? :blush: :see_no_evil:")
+            st.image("https://i.pinimg.com/564x/57/06/e8/5706e862bd4b6d479f5ec1968ade7fa9.jpg", use_column_width=True)
+            st.write("PS: If you wanna stay friends, I'm totally cool with it, just don't be awkward and all. I just wanted to show you how I feel abt u :kissing_closed_eyes:")
+
+
